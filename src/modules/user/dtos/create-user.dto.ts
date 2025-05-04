f542d3988f99613617019828ffcb52c941c8c5ae
Fabiano Lima <fabianosl@ufrrj.br>
2025-05-04 17:00:38 -0300

    fix(user): corrigindo criação de usuario

--- src/modules/user/dtos/create-user.dto.ts
+++ src/modules/user/dtos/create-user.dto.ts
@@ -1,5 +1,4 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { User } from '../user.entity';
 import { z } from 'zod';
 
 export class CreateUserDto {
@@ -11,22 +10,12 @@ export class CreateUserDto {
 
   @ApiProperty({ default: 'john@email.com' })
   email!: string;
-
-  toUser(): User {
-    const user = new User();
-
-    user.name = this.name;
-    user.password = this.password;
-    user.email = this.email;
-
-    return user;
-  }
 }
 
 export const createUserSchema = z
   .object({
     name: z.string().nonempty(),
-    passwrod: z.string().nonempty(),
+    password: z.string().nonempty(),
     email: z.string().email(),
   })
   .strict();
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -4,6 +4,7 @@ import { ZodPipe } from '@app/common/pipe/zod.pipe';
 import { UserService } from './user.service';
 import { CreateUserDto, createUserSchema } from './dtos/create-user.dto';
 import { CreateUserResponseDto } from './dtos/create-user-response.dto';
+import { User } from './user.entity';
 
 @Controller('users')
 export class UserController {
@@ -12,7 +13,16 @@ export class UserController {
   @ApiCreatedResponse({ type: CreateUserResponseDto })
   @Post('register')
   async register(@Body(new ZodPipe(createUserSchema)) createUserDto: CreateUserDto): Promise<CreateUserResponseDto> {
-    const user = await this.userService.createUser(createUserDto.toUser());
+    const user = await this.userService.createUser(this.toUser(createUserDto));
     return new CreateUserResponseDto(user);
+  }
+
+  private toUser(dto: CreateUserDto) {
+    const user = new User();
+    user.name = dto.name;
+    user.email = dto.email;
+    user.password = dto.password;
+
+    return user;
   }
 }
